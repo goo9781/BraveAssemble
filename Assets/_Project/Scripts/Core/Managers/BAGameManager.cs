c1d5c26837fad6aacb4f2ed1d337c2d5aae73cf9
MinGeon <goo9781@gmail.com>
2026-08-11 10:10:20 +0900

    [Feat] 게임 초기화 매니저 추가
    - GameManager 스크립트 추가
    - 매니저 오브젝트 추가(데이터, 게임 매니저 컴포넌트)

---
+++ Assets/_Project/Scripts/Core/Managers/BAGameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class BAGameManager : MonoBehaviour
+{
+    [SerializeField] private BADataManager _dataManager;
+
+    private bool _isInitialized;
+
+    public static BAGameManager Instance { get; private set; }
+
+    public bool IsInitialized => _isInitialized;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(InitializeGameAsync());
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+
+    private IEnumerator InitializeGameAsync()
+    {
+        if (_dataManager == null)
+        {
+            Debug.LogError("BADataManager 참조가 설정되지 않아 게임 초기화를 중단합니다.");
+            yield break;
+        }
+
+        yield return _dataManager.InitializeAsync();
+
+        if (!_dataManager.IsInitialized)
+        {
+            Debug.LogError("데이터 매니저 초기화에 실패하여 게임 초기화를 중단합니다.");
+            yield break;
+        }
+
+        _isInitialized = true;
+        Debug.Log("게임 초기화를 완료했습니다.");
+    }
+}
