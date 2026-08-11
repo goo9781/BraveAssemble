89eb81e45f96a09d78e92937adb81bf62022d357
MinGeon <goo9781@gmail.com>
2026-08-11 10:50:36 +0900

    [Feat] 전투 유닛 공통 뷰 추가
    - 유닛 GameObject와 ViewModel 바인딩 구현
    - 피해와 회복 명령 전달
    - 사망 시 오브젝트 비활성화 처리

---
+++ Assets/_Project/Scripts/Battle/Views/BAUnitView.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class BAUnitView : MonoBehaviour
+{
+    private BAUnitViewModel _viewModel;
+
+    public string Id => _viewModel?.Id;
+    public string UnitType => _viewModel?.UnitType;
+    public float CurrentHealth => _viewModel?.CurrentHealth ?? 0f;
+    public bool IsDead => _viewModel == null || _viewModel.IsDead;
+
+    public void Bind(BAUnitViewModel viewModel)
+    {
+        if (viewModel == null)
+        {
+            throw new ArgumentNullException(nameof(viewModel));
+        }
+
+        Unbind();
+
+        _viewModel = viewModel;
+        _viewModel.Died += OnDied;
+    }
+
+    public void Unbind()
+    {
+        if (_viewModel == null)
+        {
+            return;
+        }
+
+        _viewModel.Died -= OnDied;
+        _viewModel = null;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        _viewModel?.TakeDamage(damage);
+    }
+
+    public void RestoreHealth(float amount)
+    {
+        _viewModel?.RestoreHealth(amount);
+    }
+
+    public void ResetState()
+    {
+        if (_viewModel == null)
+        {
+            return;
+        }
+
+        gameObject.SetActive(true);
+        _viewModel.ResetState();
+    }
+
+    private void OnDestroy()
+    {
+        Unbind();
+    }
+
+    private void OnDied()
+    {
+        gameObject.SetActive(false);
+    }
+}
