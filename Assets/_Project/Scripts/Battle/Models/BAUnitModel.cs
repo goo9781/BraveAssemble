0a534232fecc7b373cdbf1325718fdc217a9cdd1
MinGeon <goo9781@gmail.com>
2026-08-11 10:38:43 +0900

    [Feat] 전투 유닛 상태 모델 추가
    - 유닛 데이터 기반 런타임 능력치 초기화
    - 피해, 회복 및 사망 상태 처리
    - 체력 변경과 사망 이벤트 추가

---
+++ Assets/_Project/Scripts/Battle/Models/BAUnitModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+public class BAUnitModel
+{
+    private readonly string _id;
+    private readonly string _displayName;
+    private readonly string _unitType;
+    
+    private readonly float _maxHealth;
+    private readonly float _attackDamage;
+    private readonly float _moveSpeed;
+    private readonly float _detectionRange;
+    private readonly float _attackRange;
+    private readonly float _attackInterval;
+
+    private float _currentHealth;
+    private bool _isDead;
+
+    public string Id => _id;
+    public string DisplayName => _displayName;
+    public string UnitType => _unitType;
+    public float MaxHealth => _maxHealth;
+    public float AttackDamage => _attackDamage;
+    public float MoveSpeed => _moveSpeed;
+    public float DetectionRange => _detectionRange;
+    public float AttackRange => _attackRange;
+    public float AttackInterval => _attackInterval;
+    public float CurrentHealth => _currentHealth;
+    public bool IsDead => _isDead;
+
+    public event Action<float, float> HealthChanged;
+    public event Action Died;
+
+    public BAUnitModel(BAUnitData unitData)
+    {
+        if (unitData == null)
+        {
+            throw new ArgumentNullException(nameof(unitData));
+        }
+        
+        _id = unitData.ID;
+        _displayName = unitData.DisplayName;
+        _unitType = unitData.UnitType;
+        _maxHealth = unitData.MaxHealth;
+        _attackDamage = unitData.AttackDamage;
+        _moveSpeed = unitData.MoveSpeed;
+        _detectionRange = unitData.DetectionRange;
+        _attackRange = unitData.AttackRange;
+        _attackInterval = unitData.AttackInterval;
+        _currentHealth = _maxHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (damage <= 0f || _isDead)
+        {
+            return;
+        }
+
+        _currentHealth = Math.Max(0f, _currentHealth - damage);
+
+        if (_currentHealth <= 0f)
+        {
+            _isDead = true;
+        }
+
+        HealthChanged?.Invoke(_currentHealth, _maxHealth);
+
+        if (_isDead)
+        {
+            Died?.Invoke();
+        }
+    }
+
+    public void RestoreHealth(float amount)
+    {
+        if (amount <= 0f || _isDead)
+        {
+            return;
+        }
+
+        _currentHealth = Math.Min(_currentHealth + amount, _maxHealth);
+        HealthChanged?.Invoke(_currentHealth, _maxHealth);
+    }
+
+    public void ResetState()
+    {
+        _currentHealth = _maxHealth;
+        _isDead = false;
+        HealthChanged?.Invoke(_currentHealth, _maxHealth);
+    }
+}
