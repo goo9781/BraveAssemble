9ed5f985807f2b279306cddd2b3d67ef976995a1
MinGeon <goo9781@gmail.com>
2026-08-19 16:01:40 +0900

    [Feat] 서포트 유닛 데이터 추가
    - 서포트 효과와 개별 쿨타임 데이터 모델 추가
    - 라이노 캐리어 테스트 데이터 테이블 추가
    - 일반 및 합체 상태의 효과 수치 정의

---
+++ Assets/_Project/Scripts/Data/Models/BASupportData.cs
@@ -0,0 +1,15 @@
+[System.Serializable]
+public class BASupportData : BAGameDataBase
+{
+    public string DisplayName;
+    public string PrefabKey;
+    public string EffectType;
+    public float BaseEffectValue;
+    public float Cooldown;
+    public float MoveSpeed;
+    public int NormalMaxTargetCount;
+    public float AssembledEffectMultiplier;
+    public float AssembledRange;
+    public int AssembledMaxTargetCount;
+    public float EffectDuration;
+}
