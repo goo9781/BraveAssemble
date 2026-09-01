0b069a3b319a7627f0076ecbbd9f0e7c20d67cd6
MinGeon <goo9781@gmail.com>
2026-09-01 15:04:09 +0900

    [Feat] 스테이지 브리핑 데이터 모델 추가
    - 스테이지 브리핑 JSON 모델 추가
    - 스테이지 표시 정보 필드 구성
    - 적 규모 필드 매핑

---
+++ Assets/_Project/Scripts/Data/Models/BAStageBriefingData.cs
@@ -0,0 +1,10 @@
+[System.Serializable]
+public class BAStageBriefingData : BAGameDataBase
+{
+    public string DisplayName;
+    public string EnemyInfo;
+    public string EnemyScale;
+    public string VictoryCondition;
+    public string RecommendedSupport;
+    public string RewardInfo;
+}
